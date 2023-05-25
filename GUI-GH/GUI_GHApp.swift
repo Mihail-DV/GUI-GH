15b6388d02403302b7b3aaa0e4c67423bf135d5c
Mihail-DV <76704615+Mihail-DV@users.noreply.github.com>
2023-05-26 02:18:12 +0300

    Initial Commit

---
+++ GUI-GH/GUI_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  GUI_GHApp.swift
+//  GUI-GH
+//
+//  Created by Михаил Данилин on 26.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GUI_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
